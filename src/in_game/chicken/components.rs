289dac99461394ad362ceca36a8c6898e3489001
Miroslav Valach <skvalach@gmail.com>
2023-11-25 14:24:03 +0100

    chickens sprite details

--- src/in_game/chicken/components.rs
+++ src/in_game/chicken/components.rs
@@ -1,7 +1,43 @@
 use bevy::prelude::Component;
+use bevy::sprite::Sprite;
 
 use super::quirk::Quirk;
 use super::resources::ChickenParams;
+
+pub enum BodyPart {
+    Head,
+    Wing,
+    Tail,
+    Body,
+}
+
+pub struct ChickenAnimation {
+}
+
+impl ChickenAnimation {
+    pub fn body_all()           -> Vec<i32> { vec![ 2, 3, 4, 5, 6, 7 ] }
+    pub fn wing_all()           -> Vec<i32> { vec![ 8, 9, 10 ] }
+    pub fn tail_all()           -> Vec<i32> { vec![ 11, 12, 13 ] }
+    pub fn head_all()           -> Vec<i32> { vec![ 14, 15, 16, 17, 18, 19 ] } 
+    pub fn body_run()           -> Vec<i32> { vec![ 3, 4, 5, 6] }
+    pub fn body_idle()          -> Vec<i32> { vec![ 2 ] }
+    pub fn body_sit()           -> Vec<i32> { vec![ 7 ] }
+    pub fn wing_flap()          -> Vec<i32> { vec![ 8, 9, 10 ] }
+    pub fn tail_wag()           -> Vec<i32> { vec![ 11, 12, 13 ] }
+    pub fn head_preening()      -> Vec<i32> { vec![ 18 ] }
+    pub fn head_eating()        -> Vec<i32> { vec![ 19 ] }
+    pub fn head_look_left()     -> Vec<i32> { vec![ 14 ] }
+    pub fn head_look_away()     -> Vec<i32> { vec![ 16 ] }
+    pub fn head_look_forward()  -> Vec<i32> { vec![ 15 ] }
+    pub fn head_look_right()    -> Vec<i32> { vec![ 17 ] }
+    pub fn head_rotating()      -> Vec<i32> { vec![ 14, 15, 17, 16 ] }
+}
+
+#[derive(Component)]
+pub struct ChickenPart {
+    pub part: BodyPart,
+    pub sprite: Sprite,
+}
 
 #[derive(Component)]
 pub struct Chicken {
--- src/in_game/chicken/resources.rs
+++ src/in_game/chicken/resources.rs
@@ -66,4 +66,21 @@ impl ChickenParams {
             ..default()
         }
     }
+
+    pub fn add_chicken_parts_to_atlas(
+        asset_server: Res<AssetServer>,
+        mut texture_atlases: ResMut<Assets<TextureAtlas>>
+    ) {
+        let texture_handle = asset_server.load("sprites/chicken-Sheet.png");
+        let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 32.0), 19, 1, None, None);
+        let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    }
+
+    pub fn create_idle_chicken() {
+        //let bundle = SpriteSheetBundle {
+        //    texture_atlas: texture_atlas_handle,
+        //    sprite: TextureAtlasSprite::new(1),
+        //    ..default()
+        //};
+    }
 }
