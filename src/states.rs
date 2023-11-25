a214d1dfb4eddeebc5ff1fccfe1394d1504e4c41
pali <6pali6@gmail.com>
2023-11-25 01:56:54 +0100

    Esc to quit

--- src/states.rs
+++ src/states.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, app::AppExit};
 
 #[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
 pub enum AppState {
@@ -12,16 +12,13 @@ pub fn toggle_app_state(
     mut app_next_state: ResMut<NextState<AppState>>,
     app_state: Res<State<AppState>>,
     keyboard_input: Res<Input<KeyCode>>,
+    mut app_exit_event_writer: EventWriter<AppExit>,
 ) {
-    if !keyboard_input.just_pressed(KeyCode::Escape) {
-        return;
-    }
-
-    if *app_state.get() == AppState::InGame {
-        app_next_state.set(AppState::MainMenu);
-    }
-
-    if *app_state.get() == AppState::MainMenu {
-        app_next_state.set(AppState::InGame);
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        if *app_state.get() == AppState::InGame {
+            app_next_state.set(AppState::MainMenu);
+        } else if *app_state.get() == AppState::MainMenu {
+            app_exit_event_writer.send(AppExit);
+        }
     }
 }
