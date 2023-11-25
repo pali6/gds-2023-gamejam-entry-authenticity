3536950e69180dd10029d25a15fb5e780b37ba76
pali <6pali6@gmail.com>
2023-11-25 12:24:42 +0100

    chicken menu!

--- src/in_game/chicken/mod.rs
+++ src/in_game/chicken/mod.rs
@@ -3,11 +3,14 @@ mod info_menu;
 mod quirk;
 mod resources;
 mod systems;
+mod click;
+mod debug;
 
 use super::states::InGameState;
 use crate::states::AppState;
 use bevy::prelude::*;
 use info_menu::*;
+use click::*;
 use resources::*;
 use systems::*;
 
@@ -19,6 +22,9 @@ impl Plugin for ChickenPlugin {
             // This will restart the game every time
             // we go back from the menu though :(
             .add_systems(OnEnter(AppState::InGame), spawn_chickens)
+            .add_event::<click::ChickenClickEvent>()
+            .add_plugins(InfoMenuPlugin)
+            // .add_systems(Update, debug::debug_chicken_click)
             .add_systems(
                 Update,
                 (chicken_movement, chicken_click)
