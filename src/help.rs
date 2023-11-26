833f46ded4d76aa4793149dc8e92b272dd78fa3c
pali <6pali6@gmail.com>
2023-11-26 15:01:29 +0100

    help update

--- src/help.rs
+++ src/help.rs
@@ -1,4 +1,6 @@
 pub static HELP_TEXT: &str = r#"Foxes have infilitrated your chicken coop! They have disguised themselves as chickens, and are trying to blend in.
 Fortunately you know every little quirk about your chickens and the foxes don't. By RIGHT CLICKING a chicken you can bring up a menu that will tell you about the chicken's quirks.
 If you find a chicken that is not aut-HEN-tic, you can LEFT CLICK it to kill it.
-But be careful, if you kill too many chickens, you will lose the game. If you kill all the foxes, you win the game. Good luck!"#;
+But be careful, if you kill too many chickens, you will lose the game. If you kill all the foxes, you win the game.
+And as long as there is at least one fox alive, chickens will die under mysterious circumstances every 30 seconds.
+Good luck!"#;
